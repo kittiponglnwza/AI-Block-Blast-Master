66f3bab265a189f7cbb9bc7828832dbbab65fc77
T.KITTIPONG <kitipongzaza566@gmail.com>
2026-04-27 14:15:58 +0700

    rename

---
+++ front-end/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
+      HELLO TOP 🔥
+    </div>
+  )
+}
+
+export default App
